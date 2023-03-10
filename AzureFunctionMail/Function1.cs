ee83487aa9c3f5d69bca397a2fe30dfcec02e52e
ertan-karabulut <58906185+ertan-karabulut@users.noreply.github.com>
2023-03-10 13:34:07 +0300

    firs commit

---
+++ AzureFunctionMail/Function1.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+
+namespace AzureFunctionMail
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string email = req.Query["email"];
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            Match match = regex.Match(email ?? "");
+            if (match.Success)
+                return new OkObjectResult("Valid mail adress");
+            else
+                return new OkObjectResult("Invalid mail adress");
+        }
+    }
+}
